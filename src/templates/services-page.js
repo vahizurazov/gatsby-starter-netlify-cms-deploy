f56e9f2725b68bf8cccf9caae1d97b82a0b21c7c
vahizurazov <vagiz.damir@gmail.com>
2019-11-29 18:04:10 +0200

    add page services

---
+++ src/templates/services-page.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+
+export const ServicesPageTemplate = ({ title, content, contentComponent }) => {
+  const ServicesContent = contentComponent || Content;
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <ServicesContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ServicesPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func
+};
+
+const ServicesPage = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <ServicesPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+      />
+    </Layout>
+  );
+};
+
+ServicesPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export default ServicesPage;
+
+export const servicesPageQuery = graphql`
+  query ServicesPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
