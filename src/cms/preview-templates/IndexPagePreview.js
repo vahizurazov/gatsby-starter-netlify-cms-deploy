e89f1544250bb684914bebc635f7b7470e4d1739
vahizurazov <vagiz.damir@gmail.com>
2019-12-31 11:03:55 +0200

    try to add select widget

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { IndexPageTemplate } from "../../templates/index-page";
 
-const IndexPagePreview = ({ entry, getAsset }) => {
+const IndexPagePreview = ({ entry, getAsset, widgetFor }) => {
   const data = entry.getIn(["data"]).toJS();
-
+  console.log("data indexpage", data);
   if (data) {
     return (
       <IndexPageTemplate
@@ -12,9 +12,18 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         section_1_title={data.section_1_title}
         section_1_btn_text={data.section_1_btn_text}
         section_1_subheading={data.section_1_subheading}
-        // description={data.description}
-        // intro={data.intro || { blurbs: [] }}
-        // mainpitch={data.mainpitch || {}}
+        section_2_right_subheading={data.section_2_right_subheading}
+        section_2_right_text={data.section_2_right_text}
+        section_2_left_subheading={data.section_2_left_subheading}
+        section_2_left_text={data.section_2_left_text}
+        section_2_title={data.section_2_title}
+        section_2_1_image={data.section_2_1_image}
+        section_2_2_image={data.section_2_2_image}
+        section_3_title={data.section_3_title}
+        section_3_text={data.section_3_text}
+        section_3_image_desktop={data.section_3_image_desktop}
+        section_3_image_mobile={data.section_3_image_mobile}
+        content={widgetFor("body")}
       />
     );
   } else {
