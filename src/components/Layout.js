d8e481cd4668ffda712ba3f9c2904d62493d77f9
vahizurazov <vagiz.damir@gmail.com>
2020-01-20 17:31:52 +0200

    remove favicon, change webmanifest

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,19 +19,19 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-96x96.png`}
+          href={`${withPrefix("/")}@/img/favicon-96x96.png`}
           sizes="96x96"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32.png`}
+          href={`${withPrefix("/")}@/img/favicon-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16.png`}
+          href={`${withPrefix("/")}@/img/favicon-16x16.png`}
           sizes="16x16"
         />
         <meta name="theme-color" content="#fff" />
